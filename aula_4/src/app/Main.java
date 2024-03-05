2648ff24db5f2960527aeecfcc4638effa932768
Lab02 - N°13 <Lab02 - N13@DESKTOP-8V1VIE0>
2024-03-04 21:38:29 -0300

    atualicao

--- aula_4/src/app/Main.java
+++ aula_4/src/app/Main.java
@@ -35,19 +35,24 @@ public class Main {
 						+ novoBanco.getBanco()
 						);
 			break;
+			case 2:
+				Nome = mensagem.solicitarEntrada("Digite o seu Nome");
+				CPF = mensagem.solicitarEntrada("Digite o seu CPF");
+				Celular = mensagem.solicitarEntrada("Digite o seu Celular");
+				Email = mensagem.solicitarEntrada("Digite o seu Email");
+				Senha = mensagem.solicitarEntrada("Digite a sua Senha");
+				Usuario usuario = new Usuario (Nome,CPF,Celular,Email,Senha);
+				mensagem.mostrarMensagemDeSucesso(usuario.getUsuario());				
+			break;
 			
+			case 3:
+				System.exit(0);
+		    break;
 			}
 			
-			Nome = mensagem.solicitarEntrada("Digite o seu Nome");
-			CPF = mensagem.solicitarEntrada("Digite o seu CPF");
-			Celular = mensagem.solicitarEntrada("Digite o seu Celular");
-			Email = mensagem.solicitarEntrada("Digite o seu Email");
-			Senha = mensagem.solicitarEntrada("Digite a sua Senha");
-			Usuario usuario = new Usuario (Nome,CPF,Celular,Email,Senha);
-			mensagem.mostrarMensagemDeSucesso(usuario.getUsuario());
-			opcao = "ok";
+			
 				
-		} while (opcao == "ok");
+		} while (opcaoNum == 3);
 
 	}
 
